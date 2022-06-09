ce211c990627dd7a4a27a057d26f143b2740b2b6
Mohamed Djebloun <djeblounm@gmail.com>
2022-06-09 03:57:57 +0100

    Delete nested loop I found using one loop better for css with only one class

--- script.js
+++ script.js
@@ -3,23 +3,18 @@
 function makeGrid(n=16){
     //Create an empty string variable htmlDivs
     let htmlDivs = '';
-    //For i starting from 1 to n
-    for(let i=1;i<=n;i++){
-        //Append '<div class="row">' to htmlDivs
-        htmlDivs+='<div class="row">';
-        //For j starting from 1 to n
-        for(let j=1;j<=n;j++){
-            //Append '<div class="cell"></div>' to htmlDivs
-            htmlDivs+='<div class="cell"></div>';
+    //For i starting from 1 to n*n
+    for(let i=1;i<=n*n;i++){
+        //Append '<div class="cell"></div>' to htmlDivs
+        htmlDivs+='<div class="cell"></div>';
         }
-        //Close the div
-        htmlDivs+='</div>';
-    }
     //Append htmlDivs to gridContainer
     gridContainer.innerHTML+= htmlDivs;
+    //Use css variables
+    gridContainer.style.setProperty('--grid-rows', n);
+    gridContainer.style.setProperty('--grid-cols', n);
 }
 //Create variable gridContainer that stores the grids-container div 
 const gridContainer = document.querySelector('.grids-container');
 //Call the function makeGrid
 makeGrid();
-
